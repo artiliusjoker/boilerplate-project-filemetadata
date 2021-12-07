3c70a49e0e83da4aac1105056309a191688d2bc9
Minh Quan <48386342+artiliusjoker@users.noreply.github.com>
2021-12-07 11:09:11 +0700

    add file upload

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const storage = multer.memoryStorage({
+  destination: function (req, file, callback) {
+    callback(null, "");
+  },
+});
+const upload = multer({ storage: storage });
 
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+  const fileUpload = req.file;
+  if (!fileUpload) {
+    return next(
+      new Error({ message: "Please upload a file", httpStatusCode: 400 })
+    );
+  }
+  res.json({
+    name: fileUpload.originalname,
+    type: fileUpload.mimetype,
+    size: fileUpload.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
